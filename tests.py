229b9dc1d4091435898952531df013158c6ebb37
Luke Kabat <kabat97@gmail.com>
2022-01-03 14:34:26 +0100

    Changed testing method
    Iteration count difference for different target minimum is now the main metric.

--- tests.py
+++ tests.py
@@ -1,26 +1,39 @@
 from cma import purecma as pcma
+import cma
+from colorama import init
+from colorama import Fore, Back, Style
+init()
 
-def TestCMAES(numberOfIterations, targetMinimumValue, initialSigma, standardDeviation, initialXValues, testFunction):
-    results = []
-    for it in range(0,numberOfIterations):
-        es1 = pcma.CMAES(initialXValues, initialSigma, ftarget=targetMinimumValue, randompsigma = False, randompgaussstddev = standardDeviation).optimize(testFunction, verb_disp=0)
-        es2 = pcma.CMAES(initialXValues, initialSigma, ftarget=targetMinimumValue, randompsigma = True, randompgaussstddev = standardDeviation).optimize(testFunction, verb_disp=0)
-        results.append(es1.result[1] - es2.result[1])
-    return results
-
-def EvalCMAESTest(results, endMsg = ""):
-    numOfBetterSolutions = sum([1 if x>0 else 0 for x in results])
-    print(f'Got {numOfBetterSolutions:.0f}/{len(results):.0f} solutions better than zero init' + endMsg)
-    return numOfBetterSolutions
-
-numIter = 100
-targetMin = 0.1
+numSamples = 25
 initSigma = 0.5
 initX = 8 * [0.1]
-stdDev = 10
-testFun = pcma.ff.elli
-EvalCMAESTest(TestCMAES(numIter, targetMin, initSigma, stdDev, initX, testFun), " for target = 0.1")
-EvalCMAESTest(TestCMAES(numIter, 0.05, initSigma, stdDev, initX, testFun), " for target = 0.05")
-EvalCMAESTest(TestCMAES(numIter, 0.01, initSigma, stdDev, initX, testFun), " for target = 0.01")
-EvalCMAESTest(TestCMAES(numIter, 0.001, initSigma, stdDev, initX, testFun), " for target = 0.001")
-EvalCMAESTest(TestCMAES(numIter, 0.0, initSigma, stdDev, initX, testFun), " for target = 0.0")
+stdDev = 1
+#testFun = pcma.ff.elli
+#testFun = pcma.ff.sphere
+testFun = cma.ff.rosen
+
+def CountCMAESIterations(targetMinimumValue, initialSigma, randompsigma, standardDeviation, initialXValues, testFunction):
+	es1 = pcma.CMAES(initialXValues, initialSigma, ftarget=targetMinimumValue, randompsigma = randompsigma, randompgaussstddev = standardDeviation)
+	iterCount = 0
+	while(not es1.stop()):
+		solutions = es1.ask()
+		es1.tell(solutions, [testFunction(x) for x in solutions])
+		iterCount = iterCount + 1
+	return iterCount
+
+def AverageCountCMAESIterations(samplesCount, targetMinimumValue, initialSigma, randompsigma, standardDeviation, initialXValues, testFunction):
+	iterCount = 0
+	for i in range(0, samplesCount):
+		iterCount = iterCount + CountCMAESIterations(targetMinimumValue, initialSigma, randompsigma, standardDeviation, initialXValues, testFunction)
+	iterCount = iterCount / samplesCount
+	return iterCount
+
+targetMin = 1
+while targetMin > 0.001:
+	nonRandomIters = AverageCountCMAESIterations(numSamples, targetMin, initSigma, False, stdDev, initX, testFun)
+	randomIters = AverageCountCMAESIterations(numSamples, targetMin, initSigma, True, stdDev, initX, testFun)
+	if(nonRandomIters < randomIters):
+		print(Fore.RED + str(nonRandomIters) + " < " + str(randomIters) + " [Non-random vs random] [targetMin:" + str(targetMin) + "]" + Style.RESET_ALL)
+	else:
+		print(Fore.GREEN + str(nonRandomIters) + " > " + str(randomIters) + " [Non-random vs random] [targetMin:" + str(targetMin) + "]" + Style.RESET_ALL)
+	targetMin = targetMin / 2
