4c2fd4875546770f9f11c75b055f5073823e71f9
Luke Kabat <kabat97@gmail.com>
2022-01-03 14:38:11 +0100

    Testing some optimization functions

--- tests.py
+++ tests.py
@@ -4,14 +4,6 @@ from colorama import init
 from colorama import Fore, Back, Style
 init()
 
-numSamples = 25
-initSigma = 0.5
-initX = 8 * [0.1]
-stdDev = 1
-#testFun = pcma.ff.elli
-#testFun = pcma.ff.sphere
-testFun = cma.ff.rosen
-
 def CountCMAESIterations(targetMinimumValue, initialSigma, randompsigma, standardDeviation, initialXValues, testFunction):
 	es1 = pcma.CMAES(initialXValues, initialSigma, ftarget=targetMinimumValue, randompsigma = randompsigma, randompgaussstddev = standardDeviation)
 	iterCount = 0
@@ -28,12 +20,26 @@ def AverageCountCMAESIterations(samplesCount, targetMinimumValue, initialSigma, 
 	iterCount = iterCount / samplesCount
 	return iterCount
 
-targetMin = 1
-while targetMin > 0.001:
-	nonRandomIters = AverageCountCMAESIterations(numSamples, targetMin, initSigma, False, stdDev, initX, testFun)
-	randomIters = AverageCountCMAESIterations(numSamples, targetMin, initSigma, True, stdDev, initX, testFun)
-	if(nonRandomIters < randomIters):
-		print(Fore.RED + str(nonRandomIters) + " < " + str(randomIters) + " [Non-random vs random] [targetMin:" + str(targetMin) + "]" + Style.RESET_ALL)
-	else:
-		print(Fore.GREEN + str(nonRandomIters) + " > " + str(randomIters) + " [Non-random vs random] [targetMin:" + str(targetMin) + "]" + Style.RESET_ALL)
-	targetMin = targetMin / 2
+def RunTest(targetMin):
+	while targetMin > 0.001:
+		nonRandomIters = AverageCountCMAESIterations(numSamples, targetMin, initSigma, False, stdDev, initX, testFun)
+		randomIters = AverageCountCMAESIterations(numSamples, targetMin, initSigma, True, stdDev, initX, testFun)
+		if(nonRandomIters < randomIters):
+			print(Fore.RED + str(nonRandomIters) + " < " + str(randomIters) + " [Non-random vs random] [targetMin:" + str(targetMin) + "]" + Style.RESET_ALL)
+		else:
+			print(Fore.GREEN + str(nonRandomIters) + " > " + str(randomIters) + " [Non-random vs random] [targetMin:" + str(targetMin) + "]" + Style.RESET_ALL)
+		targetMin = targetMin / 2
+
+
+initSigma = 0.5
+initX = 8 * [0.1]
+stdDev = 1
+
+numSamples = 10
+
+testFun = pcma.ff.sphere	# Min = 0 at x=(0,...,0)
+RunTest(0)
+testFun = cma.ff.rosen		# Min = 0 at x=(1,...,1)
+RunTest(1)
+
+#testFun = pcma.ff.elli
