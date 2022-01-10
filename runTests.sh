e5129e427d66695b77b78631014421c2b7ec03f7
Shrimpey <kabat97@gmail.com>
2022-01-10 12:12:12 +0100

    Added repetitions param, divided ref and random test for better csv analysis, fixed some stuff.

--- runTests.sh
+++ runTests.sh
@@ -1,34 +1,57 @@
 #!/bin/bash
 #
 # Usage:
-# bash runTests.sh -f elli,rosen,sphere,hyperelli,rastrigin,schwefel,bukin,schaffer -o out
+# bash runTests.sh -r 1 -f elli,rosen,sphere,hyperelli,rastrigin,schwefel,bukin,schaffer -o out
 #
 
 # Set decimal style as dots instead of commas
 export LC_NUMERIC="en_US.UTF-8"
 
-# Process arguments (-f for funciton names to run and -o for output file name without extension)
-while getopts f:o: flag
+# Process arguments
+#	-f for funciton names to run,
+#	-o for output file name without extension,
+#	-r repetitions
+while getopts f:o:r: flag
 do
     case "${flag}" in
         f) functions_to_run=${OPTARG};;
         o) output_file=${OPTARG};;
+        r) reps=${OPTARG};;
     esac
 done
 
+# Process list of functions suppplied (comma separated)
 IFS=', ' read -r -a functions_to_run <<< "$functions_to_run"
 
+# Sweep sigma, dimension count, xstd and estd parameters for each function
 for fun in "${functions_to_run[@]}"
 do
 	echo "Running tests for $fun function..."
-	for dims in $(seq 8 8 16)
+	for dims in $(seq 5 5 10)
 	do
-		for sigma in $(seq 0.1 0.1 0.5)
+		for sigma in $(seq 0.1 0.2 0.3)
 		do
-			# Run reference test
-			python tests.py  --sigma $sigma --dim $dims --xstart gauss --xsgm 0 --xsgstd 1 --estart dull --esgm 0 --esgstd 1 --testf elli -r 1 -o $output_file.csv
-			# Run test with random init
-			python tests.py  --sigma $sigma --dim $dims --xstart gauss --xsgm 0 --xsgstd 1 --estart gauss --esgm 0 --esgstd 1 --testf elli -r 1 -o $output_file.csv
+			for xstd in 1 10 100
+			do
+				# Run reference test
+				python tests.py  --sigma $sigma --dim $dims --xstart gauss --xsgm 0 --xsgstd $xstd --estart dull --esgm 0 --esgstd 1 --testf $fun -r $reps -o $output_file.csv
+			done
+		done
+	done
+	for dims in $(seq 5 5 10)
+	do
+		for sigma in $(seq 0.1 0.2 0.3)
+		do
+			for xstd in 1 10 100
+			do
+				for estd in 1 10 100
+				do
+					# Run test with random init
+					python tests.py  --sigma $sigma --dim $dims --xstart gauss --xsgm 0 --xsgstd $xstd --estart gauss --esgm 0 --esgstd $estd --testf $fun -r $reps -o $output_file.csv
+				done
+			done
 		done
 	done
 done
+
+
